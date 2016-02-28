286eff318424d9c84359b3ca7792bc411087e211
autocar <hizmetler@msn.com>
2016-02-29 00:30:33 +0200

    Update default.blade.php
    Changed blade syntax

--- src/autocar/LaravelFlash/resources/views/templates/bootstrap/default.blade.php
+++ src/autocar/LaravelFlash/resources/views/templates/bootstrap/default.blade.php
@@ -7,24 +7,24 @@
                 'body'       => $message['message']
             ])
         @else
-            <div class="alert alert-{{ $message['level'] }} alert-dismissible">
+            <div class="alert alert-{!! $message['level'] !!} alert-styled-left">
                 @if (is_a($message['message'], 'Illuminate\Support\MessageBag'))
-                <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                <ul>
-                    @foreach ($message['message']->all('<li>:message</li>') as $error)
-                        {{ $error }}
-                    @endforeach
-                </ul>
+                    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                    <ul>
+                        @foreach ($message['message']->all('<li>:message</li>') as $error)
+                            {!! $error !!}
+                        @endforeach
+                    </ul>
                 @else
-                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-
+                    <button type="button" class="close" data-dismiss="alert"><span>&times;</span><span class="sr-only">Close</span></button>
                     @if ( isset($message['title']) )
-                        <strong>{{ $message['title'] }}</strong>
+                        <span class="text-semibold">{!! $message['title'] !!}</span>
                     @endif
-
-                    {{ $message['message'] }}
+                    {!! $message['message'] !!}
                 @endif
             </div>
+
+
         @endif
     @endforeach
 @endif
